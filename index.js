f1db44ec601e662f7cde7c534d7082eb62235f78
Jason McGloin <jason.mcgloin@gmail.com>
2019-11-26 22:02:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+const saturdayFun = (activity = 'roller-skate') => {
+	return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = 'go to the office') => {
+	return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (hl ='*') => {
+	return (phrase) => `You are ${hl}${phrase}${hl}!`
+}
+
+let Calculator = {
+	'add' : (x,y) => x + y,
+	'subtract' : (x,y) => x - y,
+	'multiply' : (x,y) => x * y,
+	'divide' : (x,y) => x / y
+}
+
+const actionApplyer = (init, xforms) => {
+	return xforms ? xforms.reduce((a, xform) => a = xform(a), init) : init
+}
